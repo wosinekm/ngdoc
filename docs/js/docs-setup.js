36522f3083f06ab1058ed57e0b81e2fb588ba24b
Marcin Wosinek <marcin.wosinek@contractors.roche.com>
2014-02-05 12:28:25 +0000

    doc example

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,123 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "API Documentation",
+      "shortName": "API Documentation",
+      "type": "overview",
+      "moduleName": "API Documentation",
+      "shortDescription": "AngularJS API Docs",
+      "keywords": "angularjs api docs documentation overview"
+    },
+    {
+      "section": "api",
+      "id": "test1App",
+      "shortName": "test1App",
+      "type": "overview",
+      "moduleName": "test1App",
+      "shortDescription": "test1App",
+      "keywords": "api application main module overview test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.controller:MainCtrl",
+      "shortName": "MainCtrl",
+      "type": "function",
+      "moduleName": "test1App",
+      "shortDescription": "MainCtrl",
+      "keywords": "api controller function mainctrl test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.controller:UserPageCtrl",
+      "shortName": "UserPageCtrl",
+      "type": "function",
+      "moduleName": "test1App",
+      "shortDescription": "UserPageCtrl",
+      "keywords": "api controller function test1app userpagectrl"
+    },
+    {
+      "section": "api",
+      "id": "test1App.decorator:Http",
+      "shortName": "Http",
+      "type": "function",
+      "moduleName": "test1App",
+      "shortDescription": "HttpCtrl",
+      "keywords": "api decorator function httpctrl test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.directive:contenteditable",
+      "shortName": "contenteditable",
+      "type": "directive",
+      "moduleName": "test1App",
+      "shortDescription": "contenteditable",
+      "keywords": "api content contenteditable custom directive replace test1app text"
+    },
+    {
+      "section": "api",
+      "id": "test1App.filter:hideEmails",
+      "shortName": "hideEmails",
+      "type": "filter",
+      "moduleName": "test1App",
+      "shortDescription": "hideEmails",
+      "keywords": "api change filter hideemails test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.frequence",
+      "shortName": "test1App.frequence",
+      "type": "service",
+      "moduleName": "test1App",
+      "shortDescription": "frequence",
+      "keywords": "api frequence service test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.pi",
+      "shortName": "test1App.pi",
+      "type": "service",
+      "moduleName": "test1App",
+      "shortDescription": "pi",
+      "keywords": "api constat pi service test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.someFactory",
+      "shortName": "test1App.someFactory",
+      "type": "service",
+      "moduleName": "test1App",
+      "shortDescription": "someFactory",
+      "keywords": "api factory service somefactory test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.someProvider",
+      "shortName": "test1App.someProvider",
+      "type": "service",
+      "moduleName": "test1App",
+      "shortDescription": "someProvider",
+      "keywords": "api provider service someprovider test1app"
+    },
+    {
+      "section": "api",
+      "id": "test1App.SomeService",
+      "shortName": "test1App.SomeService",
+      "type": "service",
+      "moduleName": "test1App",
+      "shortDescription": "SomeService",
+      "keywords": "api service someservice test1app"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
